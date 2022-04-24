3add47f8068bc9a6310ed3d867fc02ace7a7e4e2
Jeweetzelf <67839219+jeweetzelfWasTaken@users.noreply.github.com>
2022-04-24 18:11:18 +0200

    some bug fixes

--- src/index.ts
+++ src/index.ts
@@ -3,52 +3,6 @@ class Collection<K, V> extends Map<K, V> {
         super(iterable);
     }
     
-    public add(key: K, value: V): this {
-        this.set(key, value);
-        return this;
-    }
-    
-    public delete(key: K): boolean {
-        return this.delete(key);
-    }
-    
-    public has(key: K): boolean {
-        return this.has(key);
-    }
-    
-    public get(key: K): V | undefined {
-        return this.get(key);
-    }
-    
-    public set(key: K, value: V): this {
-        this.set(key, value);
-        return this;
-    }
-    
-    public clear(): void {
-        this.clear();
-    }
-    
-    public forEach(callbackfn: (value: V, key: K, map: this) => void, thisArg?: any): void {
-        this.forEach(callbackfn, thisArg);
-    }
-    
-    public entries(): IterableIterator<[K, V]> {
-        return this.entries();
-    }
-    
-    public keys(): IterableIterator<K> {
-        return this.keys();
-    }
-    
-    public values(): IterableIterator<V> {
-        return this.values();
-    }
-    
-    public [Symbol.iterator](): IterableIterator<[K, V]> {
-        return this[Symbol.iterator]();
-    }
-
     public firstKey(): K | undefined {
         return this.keys().next().value;
     }
