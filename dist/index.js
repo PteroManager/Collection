7bf961f2d9139238addcc2dfcfad157113b69c74
Jeweetzelf <67839219+jeweetzelfWasTaken@users.noreply.github.com>
2022-04-25 11:39:38 +0200

    Fixed type things, only not for ts cuz ts sh*t

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,48 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Create a new Collection
+ * @constructor
+ * @template K, V
+ * @extends {Map<K, V>}
+ * @extends {Collection<K, V>}
+ * @implements {Collection<K, V>}
+ * @implements {Iterable<readonly [K, V]>}
+ * @implements {CollectionConstructor<K, V>}
+ * @implements {CollectionConstructor<unknown, unknown>}
+ */
 class Collection extends Map {
-    constructor(iterable = []) {
-        super(iterable);
+    /**
+     * Returns a new collection with the given key-value pairs.
+     */
+    constructor() {
+        super();
     }
+    /**
+     * Returns the first key of the collection.
+     * @returns {K} [K]
+     */
     firstKey() {
         return this.keys().next().value;
     }
+    /**
+     * Returns the first value of the collection.
+     * @returns {V} [V]
+     */
     firstValue() {
         return this.values().next().value;
     }
+    /**
+     * Returns the last key of the collection.
+     * @returns {K} [K]
+     */
     lastKey() {
         return [...this.keys()].reverse()[0];
     }
+    /**
+     * Returns the last value of the collection.
+     * @returns {V} [V]
+     */
     lastValue() {
         return [...this.values()].reverse()[0];
     }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,58 @@
+interface CollectionConstructor {
+    new(): Collection<unknown, unknown>;
+    new <K, V>(entries?: ReadonlyArray<readonly [K, V]> | null): Collection<K, V>;
+    new <K, V>(iterable: Iterable<readonly [K, V]>): Collection<K, V>;
+    readonly prototype: Collection<unknown, unknown>;
+    readonly [Symbol.species]: CollectionConstructor;
+}
+
+/**
+ * Create a new Collection
+ * @constructor
+ * @template K, V
+ * @extends {Map<K, V>}
+ * @extends {Collection<K, V>}
+ * @implements {Collection<K, V>}
+ * @implements {Iterable<readonly [K, V]>}
+ * @implements {CollectionConstructor<K, V>}
+ * @implements {CollectionConstructor<unknown, unknown>}
+ */
+
 class Collection<K, V> extends Map<K, V> {
-    constructor(iterable: Iterable<[K, V]> = []) {
-        super(iterable);
+    /**
+     * Returns a new collection with the given key-value pairs.
+     */
+    constructor() {
+        super();
     }
-    
+    /**
+     * Returns the first key of the collection.
+     * @returns {K} [K]
+     */
     public firstKey(): K | undefined {
         return this.keys().next().value;
     }
 
+    /**
+     * Returns the first value of the collection.
+     * @returns {V} [V]
+     */
     public firstValue(): V | undefined {
         return this.values().next().value;
     }
 
+    /**
+     * Returns the last key of the collection.
+     * @returns {K} [K]
+     */
     public lastKey(): K | undefined {
         return [...this.keys()].reverse()[0];
     }
 
+    /**
+     * Returns the last value of the collection.
+     * @returns {V} [V]
+     */
     public lastValue(): V | undefined {
         return [...this.values()].reverse()[0];
     }
